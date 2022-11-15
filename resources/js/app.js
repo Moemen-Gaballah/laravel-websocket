6a80fb4cf94b5e747e464350b6fa054aded50cfb
moemen gaballa <moemengaballa@gmail.com>
2022-11-15 11:22:17 +0200

    basic websocket laravel

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+
+// window.Echo.channel("Notification").listen("NotificationRecieved", event => {
+//     console.log(event);
+// });
+//
+// window.Echo.channel("Notification").listen("NotificationRecieved", event => {
+//     console.log(event);
+// });
